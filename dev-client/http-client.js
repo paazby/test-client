28a4df6e2ffc6a0bcb1fc24c785b05f30b35e6fa
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-21 11:49:21 -0700

    add ability to take command line arguments

--- dev-client/http-client.js
+++ dev-client/http-client.js
@@ -2,34 +2,18 @@ var request = require('request');
 var url = require('url');
 var qs = require('querystring');
 var jwt = require('jwt-simple');
+var argv = require('optimist').argv;
 
 var API_KEY = require('../internal-files').API_KEY;
 var JWT_SECRET = require('../internal-files').JWT_SECRET;
 var MALE_FB_ID = require('../internal-files').MALE_FB_ID;  //10154433372935473
 var FEMALE_FB_ID = require('../internal-files').FEMALE_FB_ID;
+var MY_FB_ID = require('../internal-files').MY_FB_ID;
 
-console.log(API_KEY, JWT_SECRET,MALE_FB_ID,FEMALE_FB_ID);
-
-var makeAuthenticationString = function(){
+console.log(MY_FB_ID);
+var makeAuthenticationObject = function(fbId){
   var jwtWebToken = jwt.encode({
-    fb_id: FAKE_FB_ID
-    }, JWT_SECRET);
-    
-  var apiWebToken = jwt.encode({
-    apiKey: API_KEY 
-    }, JWT_SECRET);
-
-  var keyAndToken = qs.stringify({
-    apiKey: apiWebToken,
-    token: jwtWebToken
-  });
-  
-  return keyAndToken;
-};
-
-var makeAuthenticationObject = function(){
-  var jwtWebToken = jwt.encode({
-    fb_id: FAKE_FB_ID
+    fb_id: fbId
     }, JWT_SECRET);
     
   var apiWebToken = jwt.encode({
@@ -54,36 +38,34 @@ var addQuery = function(queryObject, targetId){
 
 var makeGetRequest = function(path){ 
     
-    var host = process.env.CURRENT_HOST || 'dev-ice.cloudapp.net';
+  var host = 'dev-ice.cloudapp.net';
 
+  var options = { 
+    protocol: 'http',
+    host: host, 
+    pathname: path,
+    query: makeAuthenticationObject(MY_FB_ID)
+  };
 
-    var options = { 
-      protocol: 'http',
-      host: host, 
-      pathname: path,
-      query: makeAuthenticationObject()
-    };
+  var testUrl = url.format(options);
+  console.log('testUrl', testUrl);    
 
-    var testUrl = url.format(options);
-    console.log('testUrl', testUrl);    
-
-    request(testUrl, function(error, response, body){ 
-      if(!error && response.statusCode === 200){
-        for(var i = 0; i < 10; i++){
-          console.log(body[i]);
-        }
-      } else {
-        console.log('Error:', error);
+  request(testUrl, function(error, response, body){ 
+    if(!error && response.statusCode === 200){
+      for(var i = 0; i < 10; i++){
+        console.log(body[i]);
       }
-    });
+    } else {
+      console.log('Error:', error);
+    }
+  });
 };
 
 
 var makePostRequest = function(path){ 
     
-    var host = process.env.CURRENT_HOST || 'dev-ice.cloudapp.net';
-    var augmentedQuery = addQuery(makeAuthenticationObject(), 1);
-    console.log('aq', augmentedQuery);
+    var host = 'dev-ice.cloudapp.net';
+    var augmentedQuery = addQuery(makeAuthenticationObject(MY_FB_ID), 3);
 
     var options = { 
       protocol: 'http',
@@ -106,9 +88,11 @@ var makePostRequest = function(path){
 
 
 
+
 var runHttpClient = function(){
-  var path = process.argv[2] || '/matches';
-  var httpType = process.argv[3] || 'g';
+  var path = argv.route || '/matches';
+  var httpType = argv.method || 'g';
+  var currentId = process.id || MY_FB_ID;
   if( httpType === 'g'){
     makeGetRequest(path);
   } else if( httpType === 'p'){
